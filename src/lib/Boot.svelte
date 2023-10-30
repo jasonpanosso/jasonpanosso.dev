95cccf3f1c553370be059ba9cecb7d1c6df887c7
JasonPanosso <jasonpanosso@protonmail.com>
2023-10-30 15:18:47 -0400

    remove input from Boot.svelte, auto login as guest with simulated typing

--- src/lib/Boot.svelte
+++ src/lib/Boot.svelte
@@ -8,17 +8,20 @@
 
   export let bootCompleted = false;
 
+  let autoTypeText = 'guest';
+  let autoTypeIndex = 0;
   let showLogin = false;
-  let usernameInput = '';
+  $: usernameInput = '';
 
-  let loginInput: HTMLInputElement;
   let logContainer: HTMLElement;
   let accumulatedLogs: string[] = [];
 
   const { hostname } = get(terminalState);
 
-  $: if (showLogin && loginInput) {
-    loginInput.focus();
+  $: if (showLogin) {
+    setTimeout(async () => {
+      await simulateTyping();
+    }, 3000);
   }
 
   onMount(async () => {
@@ -50,14 +53,18 @@
     }
   }
 
-  function handleLogin(event: KeyboardEvent) {
-    if (event.key === 'Enter') {
-      terminalState.update((state) => {
-        state.username = usernameInput;
-        return state;
+  async function simulateTyping() {
+    for (let i = autoTypeIndex; i < autoTypeText.length; i++) {
+      await new Promise((resolve) => {
+        const randomDelay = Math.floor(Math.random() * (150 - 50 + 1)) + 300;
+        setTimeout(() => {
+          usernameInput += autoTypeText[i];
+          resolve(null);
+        }, randomDelay);
       });
-      bootCompleted = true;
     }
+    // TODO: Stay automatic, or put a little "ENTER" button?
+    setTimeout(() => (bootCompleted = true), 1500);
   }
 </script>
 
@@ -65,21 +72,34 @@
   bind:this={logContainer}
   out:blur={{ delay: 0, duration: 250 }}
   class="col-start-1 col-end-2 row-start-1 row-end-2 h-full overflow-hidden
-  p-4 font-unifont text-white"
+  whitespace-pre p-4 font-unifont text-white"
 >
   {#each accumulatedLogs as log}
-    <p class="whitespace-pre">{log || '\u00A0'}</p>
+    <p>{log || '\u00A0'}</p>
   {/each}
   {#if showLogin}
-    <br />
-    <p>Debian GNU/Linux 11 {hostname} tty1</p>
-    <span>{hostname} login:</span>
-    <input
-      bind:this={loginInput}
-      bind:value={usernameInput}
-      on:keydown={handleLogin}
-      on:blur={() => loginInput.focus()}
-      class="bg-black text-white outline-none"
-    />
+    <div class="flex flex-col">
+      <p>Debian GNU/Linux 11 {hostname} tty1</p>
+      <div class="flex items-center">
+        <span>{hostname} login: {usernameInput}</span>
+        <span class="fake-caret inline-block h-4 w-[1px] bg-white" />
+      </div>
+    </div>
   {/if}
 </div>
+
+<style>
+  .fake-caret {
+    animation: blink 1.5s step-end infinite;
+  }
+
+  @keyframes blink {
+    from,
+    to {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0;
+    }
+  }
+</style>
