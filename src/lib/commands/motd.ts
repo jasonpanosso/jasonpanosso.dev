856535be91d346dbbeb360a02c7abf5796e35d5b
JasonPanosso <jasonpanosso@protonmail.com>
2023-10-30 14:29:46 -0400

    add message to motd

--- src/lib/commands/motd.ts
+++ src/lib/commands/motd.ts
@@ -1,12 +1,17 @@
 import type { CommandHistoryAction } from '$lib/types';
 
-const banner = [
+const BANNER = [
   ` _    _      _                          _ `,
   `| |  | |    | |                        | |`,
   `| |  | | ___| | ___ ___  _ __ ___   ___| |`,
   `| |/\\| |/ _ \\ |/ __/ _ \\| '_ \` _ \\ / _ \\ |`,
   `\\  /\\  /  __/ | (_| (_) | | | | | |  __/_|`,
-  ` \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___(_)`,
+  ` \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___(_)\n\n`,
+];
+
+const MOTD_MESSAGE = [
+  `Thanks for visiting my site!`,
+  `Type 'help' to see a list of available commands.`,
 ];
 
 export function motdHandler(_args?: string[]): CommandHistoryAction {
@@ -14,7 +19,7 @@ export function motdHandler(_args?: string[]): CommandHistoryAction {
     type: 'ADD',
     historyItem: {
       command: 'motd',
-      output: banner,
+      output: [...BANNER, ...MOTD_MESSAGE],
     },
   };
 }
