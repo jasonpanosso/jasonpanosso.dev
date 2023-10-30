3d2913990973795e127bbfc9d210900b8be62b94
JasonPanosso <jasonpanosso@protonmail.com>
2023-10-30 14:22:52 -0400

    add iosevka, unifont fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,33 @@ import type { Config } from 'tailwindcss';
 
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+
   theme: {
     extend: {
+      fontFamily: {
+        iosevka: [
+          'Iosevka',
+          'ui-monospace',
+          'SFMono-Regular',
+          'Menlo',
+          'Monaco',
+          'Consolas',
+          'Liberation Mono',
+          'Courier New',
+          'monospace',
+        ],
+        unifont: [
+          'unifont',
+          'ui-monospace',
+          'SFMono-Regular',
+          'Menlo',
+          'Monaco',
+          'Consolas',
+          'Liberation Mono',
+          'Courier New',
+          'monospace',
+        ],
+      },
       colors: {
         primary: 'var(--primary-color)',
         secondary: 'var(--secondary-color)',
