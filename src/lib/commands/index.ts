07aaef4692352a7d71c929d86986a37032c05ead
JasonPanosso <jasonpanosso@protonmail.com>
2023-10-31 14:46:12 -0400

    remove old code

--- src/lib/commands/index.ts
+++ src/lib/commands/index.ts
@@ -1,17 +1,1 @@
 export { mapCommandToComponent } from './commandComponentMap';
-
-// export function handleInvalidCommand(
-//   invalidCommand: string
-// ): () => CommandHistoryAction {
-//   return () => ({
-//     type: 'ADD',
-//     historyItem: {
-//       command: invalidCommand,
-//       output: [
-//         invalidCommand
-//           ? `Command not found: ${invalidCommand}. Enter 'help' for a list of valid commands`
-//           : '\n',
-//       ],
-//     },
-//   });
-// }
